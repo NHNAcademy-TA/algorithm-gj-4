9ef2f05bab4b98cf6b957fb76b4d6e374339b1d0
Gyeongseo Bak <rudtj2966@gmail.com>
2023-09-03 15:09:07 +0900

    [Feat] : 0904 문제

---
+++ 09-2주차/0904/MainTest.java
@@ -0,0 +1,21 @@
+class MainTest {
+    private static Stream<Arguments> testcases() {
+        return Stream.of(
+                Arguments.of( "MANGO", "mango" ),
+                Arguments.of( "peach", "PEACH" ),
+                Arguments.of( "Apple", "aPPLE" ),
+                Arguments.of( "baNANa", "BAnanA" ),
+                Arguments.of( "Believe In YourSelF", "bELIEVE iN yOURsELf" ),
+                Arguments.of( "THiS is how LIFE is!!", "thIs IS HOW life IS!!" ),
+                Arguments.of( "Li^^ve  p$osi&TIVE*", "lI^^VE  P$OSI&tive*" ),
+                Arguments.of( "%tiMe~~~~ iS>.< gggggolD ?:)", "%TImE~~~~ Is>.< GGGGGOLd ?:)" )
+        );
+    }
+
+    @ParameterizedTest(name = "{0} --> {1}")
+    @MethodSource("testcases")
+    @DisplayName("[1] 대문자 ↔️ 소문자 변환")
+    void test(String input, String answer) {
+        assertEquals(Main.solution(input), answer);
+    }
+}
---
+++ 09-2주차/0904/README.md
@@ -0,0 +1,28 @@
+# 대문자 ↔️ 소문자 변환
+
+### [ 문제 ]
+
+- 문자열의 대문자와 소문자를 반전시킵니다.
+
+### [ 조건 ]
+
+1. [ASCII](https://www.ascii-code.com/)를 활용합니다.
+2. [java.lang.String](https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/String.html)에서 제공하는 toUpperCase(), toLowerCase()를 사용하지 않고 작성합니다.
+
+### [ 예시 ]
+
+|  입력   | -> |  출력   |
+|:-----:|----|:-----:|
+| MANGO |    | mango |
+
+|  입력   | -> |  출력   |
+|:-----:|----|:-----:|
+| peach |    | PEACH |
+
+|  입력   | -> |  출력   |
+|:-----:|----|:-----:|
+| Apple |    | aPPLE |
+
+|   입력   | -> |   출력   |
+|:------:|----|:------:|
+| baNANa |    | BAnanA |
