537d05415aa641330cceb264e4a0d61319ffa1c3
WoonSoek <81662836+uasaha@users.noreply.github.com>
2023-09-11 08:57:56 +0900

    Add files via upload

---
+++ 09-3주차/0911/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+
+    private static final int[] TEST_CASES = {
+            1,
+            60,
+            256,
+            325,
+            20_160,
+            3_456_789,
+            10_000_001,
+            1_234_567_890
+    };
+
+    private static final String[] TEST_CASES_RESULT = {
+            "1 = 1",
+            "2 * 2 * 3 * 5 = 60",
+            "2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 = 256",
+            "5 * 5 * 13 = 325",
+            "2 * 2 * 2 * 2 * 2 * 2 * 3 * 3 * 5 * 7 = 20160",
+            "3 * 7 * 97 * 1697 = 3456789",
+            "11 * 909091 = 10000001",
+            "2 * 3 * 3 * 5 * 3607 * 3803 = 1234567890"
+    };
+
+    public static void main(String[] args) {
+        for (int i = 0; i < TEST_CASES.length; i++) {
+            System.out.println("Test Case " + (i + 1) + " = " + test(TEST_CASES[i], Test_CASES_RESULT[i]));
+        }
+
+        System.out.printf("정답률 = %.3f%%", (correct / TEST_CASES.length * 100));
+    }
+
+    private static double correct = 0;
+
+    private static boolean test(int input, String result) {
+        if (solution(input).equals(result)) {
+            correct++;
+            return true;
+        }
+
+        return false;
+    }
+
+    public static String solution(int num) {
+        int origin = num;
+        List<Integer> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        
+
+        return sb.toString();
+    }
+}
